3fa50e9b8cd6ed5b56d7cff9d25b26a0c8ca5841
Nikodem <nikodem.lorenz@gmail.com>
2019-05-29 20:25:05 +0200

    Updates

--- sw.js
+++ sw.js
@@ -1,40 +1,40 @@
-importScripts('/fictional-claire/_nuxt/workbox.4c4f5ca6.js')
+importScripts('/Fictional-Claire/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/fictional-claire/_nuxt/075afef1f35cf0da3abe.js",
-    "revision": "94895d139b3569fd4ad9757cfb436ee8"
-  },
-  {
-    "url": "/fictional-claire/_nuxt/17e7f059a3ecc787a6e3.js",
+    "url": "/Fictional-Claire/_nuxt/17e7f059a3ecc787a6e3.js",
     "revision": "95a9667b4a44fe1147ca9ba9d3173f3b"
   },
   {
-    "url": "/fictional-claire/_nuxt/1ad72160985257dbd0bf.js",
+    "url": "/Fictional-Claire/_nuxt/1ad72160985257dbd0bf.js",
     "revision": "910ce25f9c09da9853cb3587e0c0cea0"
   },
   {
-    "url": "/fictional-claire/_nuxt/37956cf0b7c45671cf8b.js",
-    "revision": "886b2e744ffb39c75cb1b17596787b67"
+    "url": "/Fictional-Claire/_nuxt/291c74e09a0fb79e01ed.js",
+    "revision": "933e50e822d7f5d5a8833466b7962f17"
   },
   {
-    "url": "/fictional-claire/_nuxt/669c719cfaad98e5b4c3.js",
+    "url": "/Fictional-Claire/_nuxt/669c719cfaad98e5b4c3.js",
     "revision": "bc6239c9009deb118ce40a3c5fa09000"
   },
   {
-    "url": "/fictional-claire/_nuxt/87dbef6405f75b0942aa.js",
+    "url": "/Fictional-Claire/_nuxt/6a071f545a7ae84809e1.js",
+    "revision": "489a19594194b391c5cffed2788b61da"
+  },
+  {
+    "url": "/Fictional-Claire/_nuxt/87dbef6405f75b0942aa.js",
     "revision": "0f425645ca0237b1d9a128741bb58d4b"
   },
   {
-    "url": "/fictional-claire/_nuxt/db5cf7e6ed6081eb7090.js",
+    "url": "/Fictional-Claire/_nuxt/db5cf7e6ed6081eb7090.js",
     "revision": "da1892db4e9abd59114e54f298ea1faf"
   },
   {
-    "url": "/fictional-claire/_nuxt/ee4e5777fbbbac9715a3.js",
+    "url": "/Fictional-Claire/_nuxt/ee4e5777fbbbac9715a3.js",
     "revision": "c26e241ab76699361ae43661308a0bbf"
   },
   {
-    "url": "/fictional-claire/_nuxt/f05bb8b2024408fe34c9.js",
+    "url": "/Fictional-Claire/_nuxt/f05bb8b2024408fe34c9.js",
     "revision": "ce61d19a33e364c4298039979d15d8a6"
   }
 ], {
@@ -46,6 +46,6 @@ workbox.precaching.precacheAndRoute([
 workbox.clientsClaim()
 workbox.skipWaiting()
 
-workbox.routing.registerRoute(new RegExp('/fictional-claire/_nuxt/.*'), workbox.strategies.cacheFirst({}), 'GET')
+workbox.routing.registerRoute(new RegExp('/Fictional-Claire/_nuxt/.*'), workbox.strategies.cacheFirst({}), 'GET')
 
-workbox.routing.registerRoute(new RegExp('/fictional-claire/.*'), workbox.strategies.networkFirst({}), 'GET')
+workbox.routing.registerRoute(new RegExp('/Fictional-Claire/.*'), workbox.strategies.networkFirst({}), 'GET')
